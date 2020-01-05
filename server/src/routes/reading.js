1790d700fe54c1297fcf6d741dc3315eabdbce95
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2020-01-05 15:55:23 +0000

    add functionalitiy to save readings

--- server/src/routes/reading.js
+++ server/src/routes/reading.js
@@ -5,16 +5,17 @@ const router = express.Router();
 
 router.post("/add", (req,res) =>{
     const {data} = req.body;
-    console.dir(data);
 
     var date = new Date();
     console.log("dateTime:"+ date);
 
     var dd = date.getDate();
-    var mm = date.getMonth() + 1;
+    var mm = date.getMonth();
     var yyyy = date.getFullYear();
-    
-    console.log("date:"+dd+"/"+mm+"/"+yyyy);
+    var hour = data.time.split(":")[0];
+    var min = data.time.split(":")[1];
+
+    var dateTime = new Date(yyyy,mm,dd,hour,min);
 
     const reading = new Reading({
         deviceID: data.deviceID,
@@ -22,9 +23,54 @@ router.post("/add", (req,res) =>{
         moisture: data.moisture,
         humidity: data.humidity,
         light: data.light,
+        time: dateTime
+    });
 
-    })
-    res.status(400).json({error:"its fucked"})
+    console.log(dateTime);
+
+    reading.save()
+            .then(reading =>{
+                res.status(200).json({success:true});
+            })
+            .catch(err => res.status(400).json({ errors: err.errors }));
 });
+
+router.post("/addmany", (req,res) =>{
+    const {data} = req.body;
+
+    var hour = data.time.split(":")[0];
+    var min = data.time.split(":")[1];
+
+    for(var i=0; i < 7; i++){
+
+        var date = new Date();
+        var dd = date.getDate();
+        var mm = date.getMonth();
+        var yyyy = date.getFullYear();
+        var dateTime = new Date(yyyy,mm,dd,hour,min);
+
+        dateTime.setDate(dateTime.getDate() -i);
+        const reading = new Reading({
+            deviceID: data.deviceID,
+            temperature: data.temperature,
+            moisture: data.moisture,
+            humidity: data.humidity,
+            light: data.light,
+            time: dateTime
+        });
+        reading.save();
+    }
+
+    res.status(400).json({ errors: "its fucked"});
+})
+
+router.get("/getToday", (req, res) =>{
+    var date = new Date();
+    date.setHours(0);
+    date.setMinutes(0);
+    console.log(date)
+});
+
+
 
 export default router;
