5bf0fed2f0c06f6970c88fcec6e48a66e388d395
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2020-01-09 22:32:36 +0000

    start oneDay endpoint

--- server/src/routes/reading.js
+++ server/src/routes/reading.js
@@ -19,7 +19,7 @@ router.post("/add", (req,res) =>{
 
     const reading = new Reading({
         deviceID: data.deviceID,
-        temperature: data.temperature,
+        temperature: data.temp,
         moisture: data.moisture,
         humidity: data.humidity,
         light: data.light,
@@ -41,7 +41,7 @@ router.post("/addmany", (req,res) =>{
     var hour = data.time.split(":")[0];
     var min = data.time.split(":")[1];
 
-    for(var i=0; i < 7; i++){
+    for(var i=0; i < 3; i++){
 
         var date = new Date();
         var dd = date.getDate();
@@ -52,7 +52,7 @@ router.post("/addmany", (req,res) =>{
         dateTime.setDate(dateTime.getDate() -i);
         const reading = new Reading({
             deviceID: data.deviceID,
-            temperature: data.temperature,
+            temperature: data.temp,
             moisture: data.moisture,
             humidity: data.humidity,
             light: data.light,
@@ -64,13 +64,99 @@ router.post("/addmany", (req,res) =>{
     res.status(400).json({ errors: "its fucked"});
 })
 
-router.get("/getToday", (req, res) =>{
-    var date = new Date();
-    date.setHours(0);
-    date.setMinutes(0);
-    console.log(date)
+router.get("/getOneDay", (req, res) =>{
+    var lte = new Date();
+    lte.setHours(0);
+    lte.setMinutes(0);
+
+    var gte = new Date();
+    gte.setHours(0);
+    gte.setMinutes(0);
+    gte.setDate(gte.getDate()-1);
+    
+    var timeArray = [];
+    var averageTimes = [];
+
+    Reading.find({time:{$gte: gte.toISOString(), $lte: lte.toISOString()}})
+        .sort({time:-1})
+        .then(times =>{
+            times.forEach(time=>{
+                var newTime = {
+                    deviceID: time.deviceID,
+                    moisture: time.moisture,
+                    humidity: time.humidity,
+                    light: time.light,
+                    time: time.time
+                }
+                timeArray.push(newTime)
+            })
+            for(var i=0; i < 24; i++){
+                var time = getHourAverages(timeArray, i)
+                averageTimes.push(time)
+            }
+            res.status(200).json({ timeList: averageTimes });
+        })
+        .catch(err => {
+            console.dir(err)
+            res.status(400).json({ errors: err.errors })});
 });
 
+function getHourAverages(times, hour){
+    var chosenTimes = [];
+    times.forEach(time =>{
+        if(time.time.getHours()===hour){
+            chosenTimes.push(time)
+        }
+    })
+    if(chosenTimes.length ===0){
+        var averageReading = {
+            deviceID: 0,
+            moisture: 0,
+            temperature: 0,
+            humidity: 0,
+            light: 0,
+            time: 0
+        }
+        return(averageReading)
+    }else{
+        var average = averageReadings(chosenTimes);
+        average.time.setMinutes(0);
+        return(average);
+    }
+    
+}
 
+function averageReadings(times){
+    var len = times.length;
+    var id = times[0].deviceID;
+
+    var moisture = 0;
+    var temperature = 0;
+    var humidity = 0;
+    var light = 0;
+
+    times.forEach(time => {
+        moisture += time.moisture;
+        temperature += time.temperature;
+        humidity += time.humidity;
+        light += time.light;
+    });
+
+    moisture = moisture/len;
+    temperature = temperature/len;
+    humidity = humidity/len;
+    light = light/len;
+
+    var averageReading = {
+        deviceID: id,
+        moisture: moisture,
+        temperature: temperature,
+        humidity: humidity,
+        light: light,
+        time: times[0].time
+    }
+
+    return(averageReading)
+}
 
 export default router;
