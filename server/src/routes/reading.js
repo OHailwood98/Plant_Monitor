ab555d889facf0dab03361b55358e3accd9d3dbc
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2020-01-10 18:03:56 +0000

    create endpoint for oneweek

--- server/src/routes/reading.js
+++ server/src/routes/reading.js
@@ -41,7 +41,7 @@ router.post("/addmany", (req,res) =>{
     var hour = data.time.split(":")[0];
     var min = data.time.split(":")[1];
 
-    for(var i=0; i < 3; i++){
+    for(var i=0; i < 8; i++){
 
         var date = new Date();
         var dd = date.getDate();
@@ -83,6 +83,7 @@ router.get("/getOneDay", (req, res) =>{
             times.forEach(time=>{
                 var newTime = {
                     deviceID: time.deviceID,
+                    temperature: time.temperature,
                     moisture: time.moisture,
                     humidity: time.humidity,
                     light: time.light,
@@ -91,17 +92,59 @@ router.get("/getOneDay", (req, res) =>{
                 timeArray.push(newTime)
             })
             for(var i=0; i < 24; i++){
-                var time = getHourAverages(timeArray, i)
+                var time = getDayAverage(timeArray, i)
                 averageTimes.push(time)
             }
             res.status(200).json({ timeList: averageTimes });
         })
         .catch(err => {
             console.dir(err)
-            res.status(400).json({ errors: err.errors })});
+            res.status(400).json({ errors: err.errors })
+        });
 });
 
-function getHourAverages(times, hour){
+router.get("/getOneWeek", (req, res) =>{
+
+    var lte = new Date();
+    lte.setHours(0);
+    lte.setMinutes(0);
+
+    var gte = new Date();
+    gte.setHours(0);
+    gte.setMinutes(0);
+    gte.setDate(gte.getDate()-7);
+    gte.getM
+    
+    var timeArray = [];
+    var averageTimes = [];
+
+    Reading.find({time:{$gte: gte.toISOString(), $lte: lte.toISOString()}})
+        .sort({time:-1})
+        .then(times =>{
+            times.forEach(time=>{
+                var newTime = {
+                    deviceID: time.deviceID,
+                    temperature: time.temperature,
+                    moisture: time.moisture,
+                    humidity: time.humidity,
+                    light: time.light,
+                    time: time.time
+                }
+                timeArray.push(newTime)
+            })
+            for(var i=1; i < 8; i++){
+                var time = getWeekAverage(timeArray, lte.getDate() -i)
+                averageTimes.push(time)
+            }
+            res.status(200).json({ timeList: averageTimes });
+        })
+        .catch(err => {
+            console.dir(err)
+            res.status(400).json({ errors: err.errors })
+        });
+});
+
+function getDayAverage(times, hour){
     var chosenTimes = [];
     times.forEach(time =>{
         if(time.time.getHours()===hour){
@@ -124,6 +167,50 @@ function getHourAverages(times, hour){
         return(average);
     }
     
+}
+
+function getWeekAverage(times,day){
+    var average = [];
+    var AM6 = [];
+    var AM12 = [];
+    var PM6 = [];
+    var PM12 = [];
+
+    times.forEach(time =>{
+        if(time.time.getDate() == day){
+            if(time.time.getHours() < 6){
+                AM6.push(time);
+            }else if((time.time.getHours() >= 6)&&(time.time.getHours() < 12)){
+                AM12.push(time)
+            }else if((time.time.getHours() >= 12)&&(time.time.getHours() < 18)){
+                PM6.push(time)
+            }else{
+                PM12.push(time)
+            }
+        }
+    })
+
+    AM6 = averageReadings(AM6);
+    AM6.time.setHours(3);
+    AM6.time.setMinutes(0);
+    average.push(AM6);
+
+    AM12 = averageReadings(AM12);
+    AM12.time.setHours(9);
+    AM12.time.setMinutes(0);
+    average.push(AM12);
+
+    PM6 = averageReadings(PM6);
+    PM6.time.setHours(15);
+    PM6.time.setMinutes(0);
+    average.push(PM6);
+
+    PM12 = averageReadings(PM12);
+    PM12.time.setHours(21);
+    PM12.time.setMinutes(0);
+    average.push(PM12);
+
+    return(average)
 }
 
 function averageReadings(times){
@@ -157,6 +244,10 @@ function averageReadings(times){
     }
 
     return(averageReading)
+}
+
+function DaysinMonth(month, year){
+    return new Date(year, month, 0).getDate();
 }
 
 export default router;
