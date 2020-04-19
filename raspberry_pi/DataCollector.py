77ee9b7be63e5716fcef21a0c3bc0eac6fcae1d1
Andrew Kindleysides <andrewkindleysides@gmail.com>
2020-04-19 13:54:26 +0100

    add start script and ngrok

--- raspberry_pi/DataCollector.py
+++ raspberry_pi/DataCollector.py
@@ -21,7 +21,7 @@ device_file = device_folder + '/w1_slave'
 spi = spidev.SpiDev()
 spi.open(0,0)
 
-dataUrl = " https://fyp-plant-monitor.herokuapp.com/api/reading/add"
+dataUrl = "https://fyp-plant-monitor.herokuapp.com/api/reading/add"
  
 def read_temp_raw():
   f = open(device_file, 'r')
@@ -41,7 +41,7 @@ def read_temp():
   if equals_pos != -1:
     temp_string = lines[1][equals_pos+2:]
     temp_c = float(temp_string) / 1000.0
-    return "%0.0f" % temp_c
+    return temp_c
 
 def read_analog(channel):
   spi.max_speed_hz = 1350000
@@ -51,31 +51,29 @@ def read_analog(channel):
 
 while True:
   
-  temp1 = read_temp()
+  temp1 = float(read_temp())
   light1 = interp(read_analog(0), [0,1023], [0,100])
-  light1 = "%0.0f" % light
   moisture1 = interp(read_analog(2), [0,1023], [100,10])
-  moisture1 = "%0.0f" % moisture
   time.sleep(300)
   
-  temp2 = read_temp()
+  temp2 = float(read_temp())
   light2 = interp(read_analog(0), [0,1023], [0,100])
-  light2 = "%0.0f" % light
   moisture2 = interp(read_analog(2), [0,1023], [100,10])
-  moisture2 = "%0.0f" % moisture
   time.sleep(300)
 
-  temp3 = read_temp()
+  temp3 = float(read_temp())
   light3 = interp(read_analog(0), [0,1023], [0,100])
-  light3 = "%0.0f" % light
   moisture3 = interp(read_analog(2), [0,1023], [100,10])
-  moisture3 = "%0.0f" % moisture
   
   now = time.localtime()
   timeSTR = time.strftime("%H:%M",now)
   temp = (temp1+temp2+temp3)/3
   light = (light1+light2+light3)/3
   moisture = (moisture1+moisture2+moisture3)/3
+  
+  temp = "%0.01f" % temp
+  light = "%0.0f" % light
+  moisture = "%0.0f" % moisture
   data = {
     "data":{
       "deviceID" : "47",
---
+++ raspberry_pi/start.bash
@@ -0,0 +1,3 @@
+python DataCollector.py &
+python server.py &
+./ngrok http 5000
