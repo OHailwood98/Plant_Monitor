cb3caa49c3a4c2b3219d02b9d9a7b81293860dc9
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2020-04-20 17:06:29 +0100

    recommit changes from correct account

--- raspberry_pi/DataCollector.py
+++ raspberry_pi/DataCollector.py
@@ -6,6 +6,7 @@ import dht11
 import spidev
 from numpy import interp
 import json
+import requests
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -18,36 +19,11 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-spi = spidev.SpiDev()
-spi.open(0,0)
-
-<<<<<<< HEAD
-dataUrl = "https://fyp-plant-monitor.herokuapp.com/api/reading/add"
-=======
-instance = dht11.DHT11(pin=6)
-spi = spidev.SpiDev()
-spi.open(0,0)
->>>>>>> parent of a99a45e... add flask server to control a pump
-=======
 #instance = dht11.DHT11(pin=6)
 spi = spidev.SpiDev()
 spi.open(0,0)
 
 dataUrl = "http://192.168.0.36:8080/api/reading/add"
->>>>>>> parent of 2771134... update dataURL to match heroku URL
-=======
-#instance = dht11.DHT11(pin=6)
-spi = spidev.SpiDev()
-spi.open(0,0)
-
-dataUrl = "http://192.168.0.36:8080/api/reading/add"
->>>>>>> parent of 2771134... update dataURL to match heroku URL
-=======
-dataUrl = " https://fyp-plant-monitor.herokuapp.com/api/reading/add"
->>>>>>> parent of 77ee9b7... add start script and ngrok
  
 def read_temp_raw():
   f = open(device_file, 'r')
@@ -67,30 +43,12 @@ def read_temp():
   if equals_pos != -1:
     temp_string = lines[1][equals_pos+2:]
     temp_c = float(temp_string) / 1000.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return temp_c
-=======
-    return "%0.2f" % temp_c
-
-def read_humid():
-  result = instance.read()
-  if result.is_valid():
-    return "%0.1f" % result.humidity
->>>>>>> parent of a99a45e... add flask server to control a pump
-
-#def read_humid():
-#  result = instance.read()
-#  if result.is_valid():
-#    return "%0.0f" % result.humidity
-
-#def read_humid():
-#  result = instance.read()
-#  if result.is_valid():
-#    return "%0.0f" % result.humidity
-=======
     return "%0.0f" % temp_c
->>>>>>> parent of 77ee9b7... add start script and ngrok
+
+#def read_humid():
+#  result = instance.read()
+#  if result.is_valid():
+#    return "%0.0f" % result.humidity
 
 def read_analog(channel):
   spi.max_speed_hz = 1350000
@@ -99,67 +57,14 @@ def read_analog(channel):
   return data
 
 while True:
-<<<<<<< HEAD
-<<<<<<< HEAD
-  
-  temp1 = read_temp()
-  light1 = interp(read_analog(0), [0,1023], [0,100])
-  light1 = "%0.0f" % light
-  moisture1 = interp(read_analog(2), [0,1023], [100,10])
-<<<<<<< HEAD
-  time.sleep(30)
-=======
-  moisture1 = "%0.0f" % moisture
-  time.sleep(300)
->>>>>>> parent of 77ee9b7... add start script and ngrok
-  
-  temp2 = read_temp()
-  light2 = interp(read_analog(0), [0,1023], [0,100])
-  light2 = "%0.0f" % light
-  moisture2 = interp(read_analog(2), [0,1023], [100,10])
-<<<<<<< HEAD
-  time.sleep(30)
-=======
-  moisture2 = "%0.0f" % moisture
-  time.sleep(300)
->>>>>>> parent of 77ee9b7... add start script and ngrok
-
-  temp3 = read_temp()
-  light3 = interp(read_analog(0), [0,1023], [0,100])
-  light3 = "%0.0f" % light
-  moisture3 = interp(read_analog(2), [0,1023], [100,10])
-  moisture3 = "%0.0f" % moisture
-  
   now = time.localtime()
   timeSTR = time.strftime("%H:%M",now)
-<<<<<<< HEAD
-<<<<<<< HEAD
-  temp = (temp1+temp2+temp3)/3
-  light = (light1+light2+light3)/3
-  moisture = (moisture1+moisture2+moisture3)/3
-<<<<<<< HEAD
-  
-  temp = "%0.01f" % temp
-  light = "%0.0f" % light
-=======
-  now = time.localtime()
-  timeSTR = time.strftime("%H:%M",now)
-=======
-  now = time.localtime()
-  timeSTR = time.strftime("%H:%M",now)
->>>>>>> parent of 2771134... update dataURL to match heroku URL
   temp = read_temp()
   #humid = read_humid()
   light = interp(read_analog(0), [0,1023], [0,100])
   light = "%0.0f" % light
   moisture = interp(read_analog(2), [0,1023], [100,10])
-<<<<<<< HEAD
->>>>>>> parent of 2771134... update dataURL to match heroku URL
-=======
->>>>>>> parent of 2771134... update dataURL to match heroku URL
   moisture = "%0.0f" % moisture
-=======
->>>>>>> parent of 77ee9b7... add start script and ngrok
   data = {
     "data":{
       "deviceID" : "47",
@@ -169,36 +74,8 @@ while True:
       "light" : light,
       "moisture" : moisture
       }
-=======
-  temp = read_temp()
-  humid = read_humid()
-  light = interp(read_analog(0), [0,1023], [0,100])
-  light = "%0.1f" % light
-  data = {
-    "deviceID" : "47",
-    "time" : timeSTR,
-    "temp" : temp,
-    "humidity" : humid,
-    "light" : light
->>>>>>> parent of 7694029... remove moisture sensor (DHT11)
     }
   datSTR = json.dumps(data)
   print(datSTR)
   requests.post(url=dataUrl, json=data)
-  time.sleep(840)
-=======
-  temp = read_temp()
-  #humid = read_humid()
-  light = interp(read_analog(0), [0,1023], [0,100])
-  light = "%0.1f" % light
-  data = {
-    "deviceID" : "47",
-    "time" : timeSTR,
-    "temp" : temp,
-    #"humidity" : humid,
-    "light" : light
-    }
-  datSTR = json.dumps(data)
-  print(datSTR)
-  time.sleep(2)
->>>>>>> parent of a99a45e... add flask server to control a pump
+  time.sleep(300)
