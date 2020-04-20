e59bd4753f8a49e5bfcb40e916bc83a5c288ccff
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2020-04-20 17:03:58 +0100

    Revert "Merge branch 'master' of https://github.com/OHailwood98/Plant_Monitor"
    This reverts commit 7ce7b7cd83cbce26bd0b93e184111da5639f220c, reversing
    changes made to 277113440cd92582278ddc6ab512ef15d2aab92d.

---
+++ server/src/models/reading.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema({
+    deviceID:{type:String, required:true},
+    temperature:{type:Number},
+    moisture:{type:Number},
+    humidity:{type:Number},
+    light:{type:Number},
+    time:{type:Date, required:true}
+}, {timestamps:true});
+
+export default mongoose.model("sensor_reading",schema)
